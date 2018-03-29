b3bfe76ee12996e9ad6290b0d964fc7152a0047c
Perry Fraser <perry@frasers.org>
2018-03-29 15:58:42 -0500

    Initial commit

---
+++ src/main/kotlin/blog/pprogs/ktunion/UnionClient.kt
@@ -0,0 +1,190 @@
+package blog.pprogs.ktunion
+
+import com.beust.klaxon.*
+import okhttp3.*
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.thread
+
+fun String.encode(): String {
+    return Base64.getEncoder().encodeToString(this.toByteArray(charset("UTF-8")))
+}
+
+class Message(val op: Int, val d: Any)
+
+class Command(val name: String, val help: String)
+
+class Context(val server: Int, val socket: WebSocket, val command: Command, val args: List<String>, val sender: String, val client: UnionClient, val data: Message) {
+    fun reply(message: String) {
+        client.sendMessage(message, server)
+    }
+}
+
+@Suppress("MemberVisibilityCanBePrivate")
+class UnionClient(val selfbot: Boolean = false, val username: String, val password: String) : WebSocketListener() {
+    var servers = mutableListOf<Int>()
+    var socket: WebSocket? = null
+    var messages = mutableMapOf<String, String>()
+
+    val commands = mutableMapOf<Command, (Context) -> Unit>()
+    val clientSideCommands = mutableMapOf<Command, (List<String>) -> Unit>()
+
+    var onConnect: () -> Unit = { println("Connected") }
+    var onError: (Throwable?) -> Unit = { t -> println("Error:"); t?.printStackTrace() }
+    var onStartClosing: (Int, String?) -> Unit = { code, reason ->
+        //        socket.close(code, reason)
+        println("Closing. Code: $code, reason: $reason")
+    }
+    var onRawWSMessage: (String) -> Unit = { _ -> }
+    var onJsonWSMessage: (Message) -> Unit = { _ -> }
+    var onStartClosed: (Int, String?) -> Unit = { _, _ -> }
+    var onTextMessage: (String, String, String) -> Unit = { _, _, _ -> }
+    val onCommand: (Context) -> Boolean = { _ -> true }
+    var onStatusChange: (String, Boolean) -> Unit = { _, _ -> }
+    var onMessageDelete: (String, String) -> Unit = { _, _ -> }
+    var onBeforeMessageSent: (String) -> String = { it }
+
+    fun addCommand(command: Command, callback: (Context) -> Unit) {
+        commands[command] = callback
+    }
+
+    fun addClientSideCommand(command: Command, callback: (String) -> String) {
+//        clientSideCommands[command] = callback
+    }
+
+    //    @JvmStatic
+    fun start() {
+        val client = OkHttpClient.Builder()
+                .readTimeout(0, TimeUnit.MILLISECONDS)
+                .build()
+        val request = Request.Builder()
+                .url("ws://union.serux.pro:2082")
+                .header("Authorization", "Basic ${"$username:$password".encode()}")
+                .build()
+
+        client.newWebSocket(request, this)
+
+        // Trigger shutdown of the dispatcher's executor so this process can exit cleanly.
+        client.dispatcher().executorService().shutdown()
+
+        commands.apply {
+
+            put(Command("ping", "A simple ping command")) { ctx ->
+                ctx.reply("ur face is a pong!1!11!")
+            }
+
+            put(Command("help", "What you're reading")) { ctx ->
+                if (ctx.args.size == 1) {
+                    val result = commands.keys.firstOrNull { it.name == ctx.args.first() }
+                    if (result == null) {
+                        sendMessage("No results for that command found.")
+                    } else {
+                        sendMessage("Help description for $: ${result.help}")
+                    }
+                }
+                var final = "Here are my commands:\n\n"
+                commands.keys.forEach { final += String.format("%-20s %s%n", it.name, it.help) }
+                sendMessage(final)
+            }
+
+        }
+    }
+
+    fun send(data: Map<String, Any>, op: Int = 8) {
+        socket!!.send(Klaxon().toJsonString(Message(op, JsonObject(data))))
+    }
+
+    override fun onOpen(webSocket: WebSocket?, response: Response?) {
+        socket = webSocket
+        onConnect()
+        thread {
+            while (true) {
+                sendMessage(onBeforeMessageSent(readLine()!!))
+            }
+        }
+    }
+
+    fun sendMessage(text: String, server: Int = 1) {
+        send(mapOf("server" to server, "content" to text))
+    }
+
+    override fun onFailure(webSocket: WebSocket?, t: Throwable?, response: Response?) {
+        onError(t)
+    }
+
+    override fun onClosing(webSocket: WebSocket?, code: Int, reason: String?) {
+        webSocket?.close(code, reason)
+        onStartClosing(code, reason)
+    }
+
+    override fun onMessage(webSocket: WebSocket?, text: String?) {
+        onRawWSMessage(text!!)
+
+        val jsonConverter = object : Converter<Message> {
+            override fun fromJson(jv: JsonValue): Message {
+                return Message(jv.objInt("op"), jv.obj!!["d"]!!)
+            }
+
+            override fun toJson(value: Message): String? {
+                return """{d: ${value.d}, op: ${value.op}"""
+            }
+        }
+
+        val data = Klaxon()
+                .converter(jsonConverter)
+                .parse<Message>(text)!!
+
+        onJsonWSMessage(data)
+
+        @Suppress("UNCHECKED_CAST") // we know it'll be ok
+        when (data.op) {
+            1 -> (data.d as JsonArray<JsonObject>).forEach { servers.add(it.int("id")!!) }
+            3 -> handleMessage((data.d as JsonObject).string("author")!!, data.d.string("content")!!, data.d.int("server")!!, data, data.d.string("id")!!)
+            4 -> onStatusChange((data.d as JsonObject).string("id")!!, data.d.boolean("status")!!)
+            6 -> onMessageDelete(data.d as String, messages[data.d]!!)
+            else -> println("Message: $text")
+        }
+
+    }
+
+    private fun handleMessage(who: String, content: String, server: Int, data: Message, id: String) {
+        onTextMessage(who, content, id)
+        messages.put(id, content)
+
+        if (!content.startsWith(">")) {
+            return
+        }
+
+        if (selfbot && who != "perryprog") {
+            sendMessage("It's called a selfboat for a reason, heck off.")
+            return
+        }
+
+
+        val commandString = content.substring(1).split(" ")[0].toLowerCase()
+        val args = content.split(' ').drop(1)
+
+        val command = commands.filterKeys({ it.name == commandString })
+
+        val context = Context(
+                server,
+                socket!!,
+                command.keys.first(),
+                args,
+                who,
+                this,
+                data
+        )
+
+        if (!onCommand(context)) return
+
+        command.values.first()(context)
+    }
+
+
+    override fun onClosed(webSocket: WebSocket?, code: Int, reason: String?) {
+        onStartClosed(code, reason)
+    }
+
+
+}
---
+++ src/main/kotlin/blog/pprogs/ktunion/UnionUsage.kt
@@ -0,0 +1,85 @@
+package blog.pprogs.ktunion
+
+import com.andreapivetta.kolor.blue
+import com.andreapivetta.kolor.lightYelllow
+import com.andreapivetta.kolor.red
+import groovy.lang.Binding
+import groovy.lang.GroovyShell
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+fun main(args: Array<String>) {
+    val client = UnionClient(selfbot = true, username = "perryprog", password = "BL5-HWg-gzS-PcV")
+
+    client.onConnect = {
+        println("Connected")
+    }
+
+    client.onStatusChange = { who, online ->
+        println("* $who went ${if (online) "online" else "offline"}".lightYelllow())
+    }
+
+    client.onTextMessage = { who, content, id ->
+        println(String.format("%-100s %s", "<$who${if (who == client.username) "*" else ""}> $content", id.blue()))
+    }
+
+    client.onMessageDelete = { _, content ->
+        println("The message \"$content\" was deleted.".red())
+    }
+
+//    client.onBeforeMessageSent = {
+//
+//    }
+
+    client.addCommand(Command("hello", "Says hello")) {
+        client.sendMessage("Why are you talking to yourself.")
+    }
+
+    client.addCommand(Command("mock", "Mocks u")) {
+        client.sendMessage(
+                it.args.joinToString(" ").map {
+                    if (Math.random() > .5) it.toUpperCase() else it.toLowerCase()
+                }.joinToString(""))
+    }
+
+    client.addCommand(Command("eval", "Evals some code")) {
+        val shell = createShell(it)
+        try {
+            val result = shell.evaluate(it.args.joinToString(" "))
+            if (result == null) {
+                client.sendMessage("👌")
+            } else {
+                client.sendMessage(result.toString())
+            }
+        } catch (e: Exception) {
+            client.sendMessage("Error:\n\n$e")
+        }
+    }
+
+    client.addCommand(Command("weather", "Send the weather!")) {
+        val response = OkHttpClient().newCall(Request.Builder()
+                .url("http://wttr.in/?TQn0")
+                .header("User-Agent", "curl")
+                .build()).execute().body()!!.string()
+        it.reply("​\n$response")
+    }
+
+    client.start()
+}
+
+private fun createShell(ctx: Context): GroovyShell {
+    val binding = Binding().apply {
+        setVariable("client", ctx.client)
+        setVariable("args", ctx.args)
+        setVariable("command", ctx.command)
+        setVariable("cmd", ctx.command)
+        setVariable("sender", ctx.sender)
+        setVariable("author", ctx.sender)
+        setVariable("server", ctx.server)
+        setVariable("socket", ctx.socket)
+        setVariable("ctx", ctx)
+        setVariable("data", ctx.data)
+    }
+
+    return GroovyShell(binding)
+}
